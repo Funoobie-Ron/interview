90cec0fec4702cb2771b564d89d2a6fbf61658b6
Funoobie-Ron <funoobie@gmail.com>
2024-04-19 00:20:36 +0530

    First commit

---
+++ src/app/Pages/user-listing/user-listing.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-listing',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-listing.component.html',
+  styleUrls: ['./user-listing.component.scss']
+})
+export class UserListingComponent {
+
+}
